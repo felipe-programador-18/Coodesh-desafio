c012340307b14104ef0ed0b77330503930a65525
Felipe <machadofelipe2016@outlook.com>
2022-05-13 19:13:24 -0300

    c15-give some set up about nav

--- coodesh/src/Componentes/Navbar.js
+++ coodesh/src/Componentes/Navbar.js
@@ -1,5 +1,15 @@
 import React from 'react'
-
+//this case i dont need using context api here i want working with people
 const NavBar = () => {
-    
+    return (<nav>
+           
+            
+            <div className='container bg-secondary'>
+            <h1>Buscar Pessoas</h1>
+            </div>
+    </nav>
+)
 }
+
+
+export default NavBar
