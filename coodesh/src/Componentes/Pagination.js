f03ffb444083a51c2847ae0f565c25db57ee9906
Felipe <machadofelipe2016@outlook.com>
2022-05-16 19:15:25 -0300

    c26-change pagination

--- coodesh/src/Componentes/Pagination.js
+++ coodesh/src/Componentes/Pagination.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = (props) => {
+ const {page, totalPage, onLeftClick, onRightClick } = props
 
+ return(<div className='bg-dark d-flex flex-row align-items-center'>
+     
+          <button onClick={onLeftClick}><div>⬅</div></button>
+            <div> {page}: {totalPage} </div>
+          <button onClick={onRightClick}><div>➡</div></button>      
+           
+ </div>)
 }
 
 
