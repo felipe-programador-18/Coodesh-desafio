fe45702a0fd1f4a8ac4fbe5ae2e365d41cfc0c9d
Felipe <machadofelipe2016@outlook.com>
2022-05-16 18:52:02 -0300

    c23-created and rearrange code

--- coodesh/src/App.js
+++ coodesh/src/App.js
@@ -1,14 +1,41 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import {  GetSearchApiData,SearchUser ,LimitSearch } from './ApiRandom/api'
 import './App.css'
 import NavBar from './Componentes/Navbar'
 import SearchBar from './Componentes/SearchBar'
 
 
 function App() {
+ const ItensPorPage = 50
  const [page, setPage] = useState(0)
  const [totalPage, setTotalPage] = useState(0)
+ const [loading, setloading] = useState(false)
+ const [found, setfound] = useState(false)
+ const [pessoas, setpessoa] = useState([])
 
- // const ItensPorPage = 50
+
+ const fectchinUserApi =  async () => {
+    try {  
+      setloading(true)
+      setfound(false)
+      //create const to manage all dates!!
+      //this code passes it promisses and latter created promise all to manage all code
+      const data = await LimitSearch( ItensPorPage, ItensPorPage*page)
+      const promises = data.results.map(async(results) =>{
+         return await    SearchUser(results.url)
+      }) 
+      const getResults = await Promise.all(promises)
+      setpessoa(getResults)
+      setloading(false)
+      setTotalPage(Math.ceil(data.info / ItensPorPage))   
+    } catch (error) {
+      console.log('error in fetching!!!', error)
+    }
+ }
+
+ useEffect(() => {
+   fectchinUserApi()
+ })
   
   
   
