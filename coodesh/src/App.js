238405f757b34080d6bf1f04674342b209ceeeda
Felipe <machadofelipe2016@outlook.com>
2022-05-17 15:33:15 -0300

    c40-adding more somethings

--- coodesh/src/App.js
+++ coodesh/src/App.js
@@ -3,6 +3,7 @@ import { GetSearchApiData,SearchUser ,LimitSearch } from './ApiRandom/api'
 import './App.css'
 import NavBar from './Componentes/Navbar'
 import SearchBar from './Componentes/SearchBar'
+import { ContextProvider } from './Context/context'
 
 const FavoritosUser = 'users'
 
@@ -27,7 +28,7 @@ function App() {
       //this code passes it promisses and latter created promise all to manage all code
       const data = await LimitSearch( ItensPorPage, ItensPorPage*page)
       const promises = data.results.map(async(results) =>{
-         return await    SearchUser(results.url)
+         return await  GetSearchApiData(results.url)
       }) 
       const getResults = await Promise.all(promises)
       setpessoa(getResults)
@@ -46,21 +47,51 @@ function App() {
  //this care of loading people interesting!!!
     loadingUserFav()
  },[]) 
-
-
  useEffect(() => {
    fectchinUserApi()
  },[page])
+
+ const updatePeopleInteresting = (name) => {
+   const updatePeople =  [...interesting]
+   const peopleIndex = interesting.indexOf(name)
+   if(peopleIndex >= 0){
+      updatePeople.splice(peopleIndex,1)
+   }else{
+      updatePeople.push(name)
+   }
+   window.localStorage.setItem(FavoritosUser, JSON.stringify(updatePeople) )
+   setinteresting(updatePeople)
+ }
+
+ //create part of handling loading page
   
+ const onHandLoadUser = async (results) => {
+  if(!results){
+    fectchinUserApi()
+  }
+  setloading(true)
+  setfound(false)
+  const result = await SearchUser(results)
+  if(!result){
+    setfound(true)
+  }else{
+    setpessoa([result])
+    setPage(0)
+    setTotalPage(1)
+  }
+  setloading(false)
+ }
 
   
   
-  return (
+  return ( <ContextProvider value={{allusers: interesting, 
+     updatingpeople: updatePeopleInteresting
+     }} >
     <div className="App">
      <NavBar/>
      <SearchBar/>
     </div>
-  );
+ </ContextProvider>);
 }
 
 export default App;
