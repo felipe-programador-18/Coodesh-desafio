d2a8b89fb669c0574eb387c23d5c6fddecc32cb8
Felipe <machadofelipe2016@outlook.com>
2022-05-17 14:38:36 -0300

    c35-create updating people another parameter

--- coodesh/src/Context/context.js
+++ coodesh/src/Context/context.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const ContextManager = React.createContext({
-    allusers: [], favoritesusers
+    allusers: [],
+    updatingpeople: (id) => null
 })
 export const ContextProvider = ContextManager.Provider
 export default ContextManager
