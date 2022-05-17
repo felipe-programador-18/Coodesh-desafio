80f6b003ff3d4c9c31fb69c386b1437968ade278
Felipe <machadofelipe2016@outlook.com>
2022-05-17 15:03:02 -0300

    c38- passed function call loadinguserfav to manage

--- coodesh/src/App.js
+++ coodesh/src/App.js
@@ -1,19 +1,24 @@
 import React, {useState, useEffect} from 'react'
-import {  GetSearchApiData,SearchUser ,LimitSearch } from './ApiRandom/api'
+import { GetSearchApiData,SearchUser ,LimitSearch } from './ApiRandom/api'
 import './App.css'
 import NavBar from './Componentes/Navbar'
 import SearchBar from './Componentes/SearchBar'
 
+const FavoritosUser = 'users'
 
 function App() {
  const ItensPorPage = 50
+
  const [page, setPage] = useState(0)
  const [totalPage, setTotalPage] = useState(0)
  const [loading, setloading] = useState(false)
  const [found, setfound] = useState(false)
  const [pessoas, setpessoa] = useState([])
 
+ //it working with context
+ const [interesting, setinteresting] = useState([])
 
+  //the functions making all connection about api
  const fectchinUserApi =  async () => {
     try {  
       setloading(true)
@@ -33,11 +38,21 @@ function App() {
     }
  }
 
+ const loadingUserFav = () => {
+   const savePeople = JSON.stringify(window.localStorage.getItem(FavoritosUser))
+   setinteresting(savePeople)
+ }
+ useEffect(() =>{
+ //this care of loading people interesting!!!
+    loadingUserFav()
+ },[]) 
+
+
  useEffect(() => {
    fectchinUserApi()
  },[page])
   
-  
+
   
   
   return (
