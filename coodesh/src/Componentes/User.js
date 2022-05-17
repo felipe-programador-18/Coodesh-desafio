9f62e96629976169b414cc1d061520334d0dffcc
Felipe <machadofelipe2016@outlook.com>
2022-05-17 16:48:07 -0300

    c44-try it solve issues

--- coodesh/src/Componentes/User.js
+++ coodesh/src/Componentes/User.js
@@ -17,42 +17,27 @@ const User = (props) => {
     return (<div className='container-xl d-flex'>
             
             <div className='p-2 p-l-4'>
-                <img id='image-size' className='me-2'  src={results.picture.large} alt={results.name} />
+                <img id='image-size' className='me-2'  src={results.picture} alt={results.name} />
             </div>
 
         <div className='container d-flex flex-row p-4 justify-content-between'>
             <div className='d-flex flex-row justify-content-between align-items-center'>
                 {results.gender}
-                {results.id}
-                {results.name}
-                {results.location}
-                {results.email}
+                
             </div>
           
 
             <div className='d-flex flex-row align-items-center justify-content-between'>
                 <div className='d-flex'>
-                {results.results.map((result, index) => {
-                 return <div key={index}> {result.result.name} </div>
+                {results.name.map((names, index) => 
+                {return <div key={index}> {names.name.name} </div>
                 })}
                 </div>
 
             </div>
             
         </div>  
-      
-    
-    
-    
-    
-    
-    
-    
       <button onClick={onHandlingLetter} > {letter} </button>
-    
-    
-    
-    
     </div>
 )}
 
