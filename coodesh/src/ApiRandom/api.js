f394e794e2fb8aa97cb3b2898bdb3d664e04bbfa
Felipe <machadofelipe2016@outlook.com>
2022-05-20 18:15:21 -0300

    c66-rearrage coed

--- coodesh/src/ApiRandom/api.js
+++ coodesh/src/ApiRandom/api.js
@@ -11,7 +11,7 @@ export const SearchUser = async(results) => {
 }
 
 //here boudaries fielt of search
-export const LimitSearch = async(page=0,seed='',results=50) => {
+export const LimitSearch = async(page=0,seed=1,results=50) => {
    try {
        let url =`https://randomuser.me/api?/page=?${page}&results=${results}&seed=${seed}`
        //&seed=${seed}
