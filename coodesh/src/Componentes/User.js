07621782b6f4d5138cd651543438200b6f4f36c6
Felipe <machadofelipe2016@outlook.com>
2022-05-20 20:28:10 -0300

    c68-veryfi some issues

--- coodesh/src/Componentes/User.js
+++ coodesh/src/Componentes/User.js
@@ -4,6 +4,7 @@ import ContextManager from '../Context/context'
 const User = (props) => {
     const {allusers, updatingpeople} = useContext(ContextManager)
     const {results} = props;
+    const letter = allusers.includes(results.name) ?  "📜" : "📃"
     
     const onHandlingLetter = () =>{
         // make some test inside here
@@ -11,25 +12,24 @@ const User = (props) => {
     }
   
     
-    const letter = allusers.includes(results.name) ?  "📜" : "📃"
     
     return (<div className='container-xl d-flex'>
             
             <div className='p-2 p-l-4'>
-                <img id='image-size' className='me-2'  alt={results.name} src={results.picture} />
+                <img id='image-size' className='me-2'  alt={results.results.name} src={results.picture} />
             </div>
 
         <div className='container d-flex flex-row p-4 justify-content-between'>
             <div className='d-flex flex-row justify-content-between align-items-center'>
                <h3> {results.name} </h3>
-                <div className='container'>{results.id} </div>
+                <div className='container'> #{results.id} </div>
             </div>
           
 
             <div className='d-flex flex-row align-items-center justify-content-between'>
                 <div className='d-flex'>
-                {results.gender.map((info, index) => 
-                {return <div key={index} className='container'> {info.info.name} </div>
+                {results.results.map((name, index) => 
+                {return <div key={index} className='container'> {name.name.results} </div>
                 })}
                 </div>
 
