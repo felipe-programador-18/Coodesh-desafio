99ca65cf1f6c31ddbb2e9b1d014facd8a9948a4d
Felipe <machadofelipe2016@outlook.com>
2022-06-21 18:24:48 -0300

    c135-testing some dependence inside index

--- coodesh/src/index.js
+++ coodesh/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import  'bootstrap/dist/css/bootstrap.css'
 import Modal from 'react-modal'
-
+ 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.14.3/react-modal.min.js"
 integrity="sha512-MY2jfK3DBnVzdS2V8MXo5lRtr0mNRroUI9hoLVv2/yL3vrJTam3VzASuKQ96fLEpyYIT4a8o7YgtUs5lPjiLVQ=="
 crossorigin="anonymous"
